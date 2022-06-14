122d26672b7bb4badef6b527223b4276982cc4eb
mari-utng <xti_55@hotmail.com>
2022-06-14 17:41:59 -0500

    curso

---
+++ basico/modulo3/lab311110.py
@@ -0,0 +1,7 @@
+word = input ('Ingrese el nombre de planta')
+if word == "ESPATIFILIO":
+    print ("Si, ¡El ESPATIFILIO! es la mejor planta de todos los tiempos!")
+if word == "espatifilo":
+    print ("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print ("¡ESPATIFILIO!, ¡No [entrada]!")
---
+++ basico/modulo3/lab3114.py
@@ -0,0 +1,5 @@
+num =input('ingresa un numero')
+num = int (num) #convierte a entero
+
+print (num < 100)
+print (num >= 100)
